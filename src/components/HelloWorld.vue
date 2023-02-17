663dd45878c5156ded06083e82259bf6c2b2d258
John Tönnäng <john_tonnang@hotmail.com>
2023-02-17 11:56:17 +0100

    Add video background with playback rate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,17 +5,35 @@
         required: true,
         type: String
       }
+    },
+    mounted() {
+      this.slowDown()
+    },
+    methods: {
+      slowDown() {
+        const videoPlayer = this.$refs.videoBackground
+        videoPlayer.playbackRate = 0.5
+      }
     }
   }
 </script>
+
+<template>
+  <video ref="videoBackground" autoplay muted loop id="hiker-background">
+    <source
+      id="video-source"
+      src="/assets/hiker-background.mp4"
+      type="video/mp4"
+    />
+  </video>
+  <p>{{ msg }}</p>
+</template>
 
 <style>
   p {
     font-weight: bold;
   }
+  #hiker-background {
+    max-width: 70%;
+  }
 </style>
-
-<template>
-  <img alt="" src="/assets/fox.jpeg" />
-  <p>{{ msg }}</p>
-</template>
