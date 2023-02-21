ec3d5428bf2634286a16ae1672defe9a959eb57f
Thitisuda Mahasrithanet <thiti.m90@gmail.com>
2023-02-21 15:46:39 +0100

    Add new components and views for favourites

---
+++ src/components/FavouriteSection.vue
@@ -0,0 +1,31 @@
+<script>
+  export default {
+    data() {
+      return {
+        products: null
+      }
+    },
+    created() {
+      fetch('/assets/api.JSON')
+        .then((response) => response.json())
+        .then((result) => {
+          this.products = result
+          console.log(result)
+        })
+    }
+  }
+</script>
+
+<template>
+  <main>
+    <p id="page-direction">Hem / Wishlist</p>
+    <h1>My Favourites</h1>
+    <div id="products-div" :key="product.id" v-for="product in products" />
+  </main>
+</template>
+
+<style scoped>
+  body {
+    max-width: 100%;
+  }
+</style>
--- src/views/FavouriteView.vue
+++ src/views/FavouriteView.vue
@@ -1,1 +1,19 @@
-<template><h1>allo word</h1></template>
+<script>
+  import FavouriteSection from '../components/FavouriteSection.vue'
+  // import CaruselBig from '../components/CaruselBig.vue'
+  // import CaruselProducts from '../components/CaruselProducts.vue'
+
+  export default {
+    components: {
+      FavouriteSection
+      // CaruselBig,
+      // CaruselProducts
+    }
+  }
+</script>
+
+<template>
+  <FavouriteSection />
+  <!-- <carusel-big />
+  <carusel-products /> -->
+</template>
