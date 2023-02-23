54538ec8f3647ce5f3202156219bc2c2426060d7
Johan Leijon <leijon.96@hotmail.com>
2023-02-23 19:27:32 +0100

    Creating base for profile page

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,3 +1,430 @@
 <template>
-  <h1>Profile</h1>
+  <main>
+    <div id="main-container">
+      <div id="menu-container">
+        <div id="points-container">
+          <h2 style="font-size: 3rem; font-weight: 800">Poäng: 1337</h2>
+          <img src="/assets/profile-points.png" alt="" />
+          <p>
+            Du behöver 1337 poäng till för att få din nästa bonusrabatt, och
+            1864 till för att bli Plus-1337-medlem. Bonusrabatter utfärdas med
+            en fördräjning på 30 dagar.
+          </p>
+        </div>
+        <div id="discount-container">
+          <div class="discount-container-box">
+            <img src="/assets/snel-hezt.jpg" alt="" />
+            <h3>Medlemmar får 20% på linnement för hästar!</h3>
+            <p>Giltig till och med: 17/11-2023</p>
+          </div>
+          <div class="discount-container-box">
+            <img src="/assets/snel-hezt.jpg" alt="" />
+            <h3>Medlemmar får 20% på linnement för hästar!</h3>
+            <p>Giltig till och med: 17/11-2023</p>
+          </div>
+          <div class="discount-container-box">
+            <img src="/assets/snel-hezt.jpg" alt="nej" />
+            <h3>Medlemmar får 20% på linnement för hästar!</h3>
+            <p>Giltig till och med: 17/11-2023</p>
+          </div>
+          <div class="discount-container-box">
+            <img src="/assets/snel-hezt.jpg" alt="" />
+            <h3>Medlemmar får 20% på linnement för hästar!</h3>
+            <p>Giltig till och med: 17/11-2023</p>
+          </div>
+        </div>
+        <div id="profile-options-container">
+          <div class="option-container oc-2">
+            <h4 style="margin-bottom: 0px">Mina beställningar</h4>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </div>
+          <div class="option-container">
+            <h4 style="margin-bottom: 0px">Mina poäng</h4>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </div>
+          <div class="option-container oc-2">
+            <h4 style="margin-bottom: 0px">Kontoinställningar</h4>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </div>
+          <div class="option-container">
+            <h4 style="margin-bottom: 0px">Bjud in en vän</h4>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </div>
+          <div class="option-container oc-2">
+            <h4 style="margin-bottom: 0px">
+              Investera i Maximum Entertainment
+            </h4>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </div>
+          <div class="option-container">
+            <h4 style="margin-bottom: 0px">Kontakta oss</h4>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </div>
+          <div class="option-container oc-2">
+            <h4 style="margin-bottom: 0px">Logga ut</h4>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div id="order-container">
+        <h1 style="font-weight: 800">Hej Göran!</h1>
+        <h3>Dina tidigare beställningar:</h3>
+        <div class="prev-order-container">
+          <div class="order-content">
+            <h4>Beställning</h4>
+            <p style="margin-left: 2px">5 antal varor</p>
+          </div>
+          <div class="order-content">
+            <p>1337,50kr</p>
+            <p>01/01/2023</p>
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="28"
+            height="28"
+            fill="currentColor"
+            class="bi bi-chevron-right"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+            />
+          </svg>
+        </div>
+        <div class="prev-order-container">
+          <div class="order-content">
+            <h4>Beställning</h4>
+            <p>5 antal varor</p>
+          </div>
+          <div class="order-content">
+            <p>1337,50kr</p>
+            <p>01/01/2023</p>
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="28"
+            height="28"
+            fill="currentColor"
+            class="bi bi-chevron-right"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+            />
+          </svg>
+        </div>
+        <div class="prev-order-container">
+          <div class="order-content">
+            <h4>Beställning</h4>
+            <p>5 antal varor</p>
+          </div>
+          <div class="order-content">
+            <p>1337,50kr</p>
+            <p>01/01/2023</p>
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="28"
+            height="28"
+            fill="currentColor"
+            class="bi bi-chevron-right"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+            />
+          </svg>
+        </div>
+        <div class="prev-order-container">
+          <div class="order-content">
+            <h4>Beställning</h4>
+            <p>5 antal varor</p>
+          </div>
+          <div class="order-content">
+            <p>1337,50kr</p>
+            <p>01/01/2023</p>
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="28"
+            height="28"
+            fill="currentColor"
+            class="bi bi-chevron-right"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+            />
+          </svg>
+        </div>
+        <h1>Vill du returnera en vara? Synd. Det får du inte.</h1>
+      </div>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+  body {
+    width: 95%;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  main {
+    position: relative;
+    top: 130px;
+  }
+
+  main p {
+    width: 95%;
+    margin: 0 auto;
+  }
+
+  #main-container {
+    width: 95%;
+    display: flex;
+    margin: 0 auto;
+    margin-bottom: 50px; /* Ska bort sen? */
+  }
+
+  #menu-container {
+    width: 30%;
+  }
+
+  #points-container {
+    padding: 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #points-container img {
+    width: 90%;
+  }
+
+  #points-container p {
+    margin-top: 20px;
+    text-align: left;
+    font-weight: 600;
+  }
+
+  #discount-container {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    font-size: 0.8em;
+    margin-bottom: 25px;
+  }
+
+  .discount-container-box:hover {
+    cursor: pointer;
+    opacity: 0.6;
+    box-shadow: 0px 0px 20px black;
+  }
+  .discount-container-box {
+    background-color: #79d681;
+    width: 200px;
+    margin-top: 30px;
+    border-radius: 8px;
+    height: 300px;
+  }
+
+  .discount-container-box img {
+    width: 100%;
+    height: 60%;
+    object-fit: cover;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+  }
+
+  .discount-container-box h3 {
+    font-weight: 600;
+    font-size: 1.2rem;
+    margin-top: 10px;
+    padding: 5px 15px 0px 15px;
+  }
+
+  .discount-container-box p {
+    padding: 0 10px;
+  }
+  .option-container {
+    display: flex;
+    align-items: center;
+    padding: 15px;
+    margin: 15px 0;
+    border-radius: 5px;
+    background-color: rgb(241, 241, 241);
+    font-size: 0.5rem;
+  }
+
+  .option-container:hover {
+    cursor: pointer;
+    background-color: black;
+    color: white;
+    transition: 0.3s ease-in-out;
+  }
+
+  .option-container h4 {
+    font-size: 1rem;
+    font-weight: 600;
+  }
+  .option-container svg {
+    margin-left: auto;
+  }
+
+  #order-container {
+    width: 70%;
+    margin-left: 7%;
+    padding-top: 15px;
+  }
+
+  #order-container h3 {
+    font-size: 1.3rem;
+    font-weight: 600;
+    margin-top: 25px;
+    color: rgb(46, 46, 46);
+  }
+  .prev-order-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 70%;
+    box-shadow: 1px 1px 6px black;
+    border-radius: 8px;
+    padding: 25px;
+    margin: 20px 0 40px 0px;
+    background: linear-gradient(0.25turn, #fcfcfc, #f9faf9, #f3f2f0);
+  }
+  .prev-order-container:hover {
+    cursor: pointer;
+    opacity: 0.4;
+  }
+  .order-content h4 {
+    font-weight: 600;
+    margin-bottom: 0px;
+  }
+
+  .order-content p {
+    color: #5a5a5a;
+    margin-top: 10px;
+  }
+
+  @media (max-width: 1600px) {
+    #menu-container {
+      width: 40%;
+    }
+    #order-container {
+      width: 60%;
+    }
+  }
+
+  @media (max-width: 1200px) {
+    #menu-container {
+      width: 50%;
+    }
+    #order-container {
+      width: 50%;
+    }
+    .prev-order-container {
+      width: 90%;
+    }
+  }
+
+  @media (max-width: 960px) {
+    #menu-container {
+      width: 25%;
+    }
+    #order-container {
+      width: 75%;
+    }
+  }
+</style>
