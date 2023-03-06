7e791747b88383f0dfbc70ce7a197ba72cb4927c
Hanne-Kristin-Steensath <hanne.kristin.steensath@gmail.com>
2023-03-06 13:03:10 +0100

    add CheckoutInput.vue (moved input in a component)  and OrderConfirmation.vue

--- src/components/CheckoutInput.vue
+++ src/components/CheckoutInput.vue
@@ -1,13 +1,5 @@
 <script>
-  import LoginProfile from '../components/LoginProfile.vue'
-  import CheckoutInput from '../components/CheckoutInput.vue'
-
   export default {
-    components: {
-      LoginProfile,
-      CheckoutInput
-    },
-
     data() {
       return {
         name: '',
@@ -28,13 +20,15 @@
         existingUser: false,
         profileName: '',
         profileEmail: '',
-        profilePhone: '',
-        addPoints: 0,
-        loginTrue: false
+        profilePhone: ''
       }
     },
 
-    computed: {},
+    computed: {
+      total() {
+        return this.cartItems.reduce((x, item) => x + item.price, 0)
+      }
+    },
     mounted() {
       const existingUser = localStorage.getItem('existing-user')
       const registredUser = localStorage.getItem('registred-user')
@@ -86,51 +80,30 @@
         this.saveData = true
       }
     },
-    watch: {},
+    watch: {
+      cartItems: {
+        handler: function (value) {
+          localStorage.setItem('Cart', JSON.stringify(value))
+        },
+        deep: true
+      }
+    },
     methods: {
-      loginActive() {
-        if (!this.loginTrue) {
-          this.loginTrue = true
-        } else {
-          this.loginTrue = false
-        }
+      submitOrder() {
+        // Sende til server som vi ikke har :)
+        this.name = ''
+        this.email = ''
+        this.phone = ''
+        this.address = ''
+        this.city = ''
+        this.state = ''
+        this.zip = ''
+        this.card = ''
+        this.exp = ''
+        this.cvv = ''
       },
 
-      addOrderToProfile() {
-        if (
-          this.address &&
-          this.city &&
-          this.state &&
-          this.zip &&
-          this.card &&
-          this.exp &&
-          this.cvv
-        ) {
-          if (localStorage.getItem('Orders')) {
-            this.order = JSON.parse(localStorage.getItem('Cart'))
-            this.orders = JSON.parse(localStorage.getItem('Orders'))
-            let profilePoints = Number(localStorage.getItem('ProfilePoints'))
-            for (let i = 0; i < this.order.length; i++) {
-              profilePoints += 197
-            }
-            localStorage.setItem('ProfilePoints', profilePoints)
-
-            this.orders.push(this.order)
-            localStorage.setItem('Orders', JSON.stringify(this.orders))
-            localStorage.removeItem('Cart')
-            console.log('hej')
-          } else {
-            this.order = JSON.parse(localStorage.getItem('Cart'))
-            for (let i = 0; i < this.order.length; i++) {
-              this.addPoints += 197
-            }
-            localStorage.setItem('ProfilePoints', this.addPoints)
-            this.orders.unshift(this.order)
-            localStorage.setItem('Orders', JSON.stringify(this.orders))
-            localStorage.removeItem('Cart')
-            console.log('hej')
-          }
-        }
+      sendData() {
         const data = {
           name: this.name,
           email: this.email,
@@ -150,56 +123,123 @@
         }
       }
     }
+    /*
+    submitOrder() {
+      // Sende til server som vi ikke har :)
+      this.name = ''
+      this.email = ''
+      this.phone = ''
+      this.address = ''
+      this.city = ''
+      this.state = ''
+      this.zip = ''
+      this.card = ''
+      this.exp = ''
+      this.cvv = ''
+    }
+*/
+    /*saveInfo() {
+      localStorage.setItem(
+        'checkoutInfo',
+        JSON.stringify({
+          name: this.name,
+          email: this.email,
+          phone: this.phone,
+          address: this.address,
+          city: this.city,
+          state: this.state,
+          zip: this.zip,
+          card: this.card,
+          exp: this.exp,
+          cvv: this.cvv
+        })
+      )
+      localStorage.name = this.name
+    }*/
   }
 </script>
 <template>
-  <main>
-    <div
-      v-if="loginTrue"
-      class="login-container"
-      :class="{ 'login-container-active': loginTrue }"
-    >
-      <svg
-        class="svg-icon"
-        @click="loginActive()"
-        style="
-          width: 32px;
-          height: 32px;
-          vertical-align: middle;
-          fill: currentColor;
-          overflow: hidden;
-          margin-left: auto;
-          margin-top: 3px;
-        "
-        viewBox="0 0 1024 1024"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M810.65984 170.65984q18.3296 0 30.49472 12.16512t12.16512 30.49472q0 18.00192-12.32896 30.33088l-268.67712 268.32896 268.67712 268.32896q12.32896 12.32896 12.32896 30.33088 0 18.3296-12.16512 30.49472t-30.49472 12.16512q-18.00192 0-30.33088-12.32896l-268.32896-268.67712-268.32896 268.67712q-12.32896 12.32896-30.33088 12.32896-18.3296 0-30.49472-12.16512t-12.16512-30.49472q0-18.00192 12.32896-30.33088l268.67712-268.32896-268.67712-268.32896q-12.32896-12.32896-12.32896-30.33088 0-18.3296 12.16512-30.49472t30.49472-12.16512q18.00192 0 30.33088 12.32896l268.32896 268.67712 268.32896-268.67712q12.32896-12.32896 30.33088-12.32896z"
-        />
-      </svg>
-      <LoginProfile />
+  <div class="mainContainer" :class="{ 'main-login-active': loginTrue }">
+    <div class="checkoutcontainer">
+      <h1 class="h1Checkout">Checkout</h1>
+
+      <form class="formContainer" @submit.prevent="submitOrder">
+        <fieldset class="custom">
+          <legend>Custom Information</legend>
+          <label for="name">Name:</label>
+          <input type="text" id="name" v-model="profileName" required />
+
+          <label for="email">Email:</label>
+          <input type="email" id="email" v-model="profileEmail" required />
+
+          <label for="phone">Phone:</label>
+          <input type="tel" id="phone" v-model="profilePhone" required />
+        </fieldset>
+
+        <fieldset class="shipping">
+          <legend>Shipping Details</legend>
+          <label for="address">Address:</label>
+          <textarea id="address" v-model="address" required />
+
+          <label for="city">City:</label>
+          <input type="text" id="city" v-model="city" required />
+
+          <label for="state">State:</label>
+          <input type="text" id="state" v-model="state" required />
+
+          <label for="zip">ZIP Code:</label>
+          <input type="text" id="zip" v-model="zip" required />
+        </fieldset>
+
+        <fieldset class="payment">
+          <legend>Payment Information</legend>
+          <label for="card">Credit Card Number:</label>
+          <input type="text" id="card" v-model="card" required />
+
+          <label for="exp">Expiration Date:</label>
+          <input type="month" id="exp" v-model="exp" required />
+
+          <label for="cvv">CVV:</label>
+          <input type="text" id="cvv" v-model="cvv" required />
+          <PaymentInput />
+        </fieldset>
+
+        <div class="saveInfo">
+          <input type="checkbox" v-model="saveData" />
+          <p class="saveInfoP">Save my Information</p>
+        </div>
+        <RouterLink to="/ConfirmationView">
+          <button class="submitButton" @click="addOrderToProfile" type="submit">
+            Place Order
+          </button>
+        </RouterLink>
+      </form>
     </div>
-    <div class="mainContainer" :class="{ 'main-login-active': loginTrue }">
-      <div class="checkoutcontainer">
-        <div class="login-check-container" v-if="existingUser">
-          <h2>Logged in as</h2>
-          <span>
-            <RouterLink to="/Profile">{{ profileName }}</RouterLink>
-          </span>
-        </div>
+    <div class="shoppingcartPreview">
+      <div class="container">
+        <h1 class="previewh">Shopping cart preview</h1>
+        <div class="cartBox">
+          <div
+            class="cartItems"
+            v-for="(item, index) in cartItems"
+            :key="index"
+          >
+            <div class="itemRow">
+              <div>
+                <img class="productImg" :src="item.image" :alt="item.name" />
+              </div>
 
-        <div class="login-check-container" v-else>
-          <h2>Already have an account?</h2>
-          <span @click="loginActive()"> Log in</span>
+              <div class="productInfo">
+                <span class="name">{{ item.name }}</span>
+                <span class="price">{{ item.price }} :- </span>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <form class="formContainer" @submit.prevent="submitOrder" />
+        <div class="total">Total: {{ total }} :-</div>
       </div>
     </div>
-    <checkout-input class="checkoutInput" />
-  </main>
+  </div>
 </template>
 
 <style scoped>
@@ -210,9 +250,6 @@
     font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS',
       sans-serif;
     margin-bottom: 10rem;
-  }
-  .checkoutInput {
-    padding-bottom: 5rem;
   }
 
   .main-login-active {
---
+++ src/views/ConfirmationView.vue
@@ -0,0 +1,38 @@
+<script>
+  import OrderConfirmation from '../components/OrderConfirmation.vue'
+
+  export default {
+    data() {
+      return {}
+    },
+    components: {
+      OrderConfirmation
+    }
+  }
+</script>
+
+<template>
+  <main>
+    <order-confirmation class="orderConformation" />
+  </main>
+</template>
+
+<style scoped>
+  main {
+    position: relative;
+    top: 150px;
+    width: 100%;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS',
+      sans-serif;
+    margin-bottom: 45vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .orderConformation {
+    margin-top: 5rem;
+  }
+  .loginProfile {
+    display: none;
+  }
+</style>
